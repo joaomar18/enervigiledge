858dd48222bf0242ea8457f50c077e95e8db2db0
joaomar18 <joao.marques18jm@gmail.com>
2025-04-01 15:07:00 +0100

    up

--- util/debug.py
+++ util/debug.py
@@ -1,4 +1,5 @@
 import logging
 
 logging.basicConfig(level=logging.INFO)
-logger = logging.getLogger()
+logging.getLogger("pymodbus").setLevel(logging.CRITICAL)
+logger = logging.getLogger("enervigil_edge")
