93ff3ceedb42aed86133e032e0ed53606155f1b6
joaomar18 <joao.marques18jm@gmail.com>
2025-08-04 20:04:10 +0100

    up

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ from controller.manager import DeviceManager
 from web.server import HTTPServer
 from util.debug import LoggerManager
 
-#from data.nodes import get_orno_we_516_db, get_sm1238_db
+# from data.nodes import get_orno_we_516_db, get_sm1238_db
 
 #######################################
 
@@ -34,8 +34,8 @@ async def async_main():
     # Create core infrastructure
     timedb_client = TimeDBClient()
     sqlitedb_client = SQLiteDBClient()
-    #sqlitedb_client.insert_energy_meter(get_orno_we_516_db())
-    #sqlitedb_client.insert_energy_meter(get_sm1238_db())
+    # sqlitedb_client.insert_energy_meter(get_orno_we_516_db())
+    # sqlitedb_client.insert_energy_meter(get_sm1238_db())
     mqtt_client = MQTTClient(config_file="mqtt/client_options.env")
     device_manager = DeviceManager(publish_queue=mqtt_client.publish_queue, measurements_queue=timedb_client.write_queue, devices_db=sqlitedb_client)
     http_server = HTTPServer(host="0.0.0.0", port=8000, device_manager=device_manager, db=sqlitedb_client, timedb=timedb_client)
